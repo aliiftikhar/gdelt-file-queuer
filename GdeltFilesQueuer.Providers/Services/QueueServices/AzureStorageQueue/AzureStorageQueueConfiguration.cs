26d60146df9eec39e7b4be636ba392d49bafbb43
M. Ali Iftikhar <ali.iftikhar@outlook.com>
2020-10-15 16:27:50 +0100

    azure storage queue added as the provider for the application

---
+++ GdeltFilesQueuer.Providers/Services/QueueServices/AzureStorageQueue/AzureStorageQueueConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace GdeltFilesQueuer.Providers.Services.QueueServices.AzureStorageQueue
+{
+    public class AzureStorageQueueConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string QueueName { get; set; }
+    }
+}
---
+++ GdeltFilesQueuer.Providers/Services/QueueServices/AzureStorageQueue/AzureStorageQueueService.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Queues;
+using GdeltFilesQueuer.Core.Services.QueueService;
+using Microsoft.Extensions.Options;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GdeltFilesQueuer.Providers.Services.QueueServices.AzureStorageQueue
+{
+    public class AzureStorageQueueService : IQueueService
+    {
+        private readonly AzureStorageQueueConfiguration azureStorageQueueConfiguration;
+        private readonly QueueClient queueClient;
+
+        public AzureStorageQueueService(IOptions<AzureStorageQueueConfiguration> azureStorageQueueConfiguration)
+        {
+            this.azureStorageQueueConfiguration = azureStorageQueueConfiguration?.Value ?? throw new ArgumentNullException(nameof(azureStorageQueueConfiguration));
+
+            if (string.IsNullOrWhiteSpace(this.azureStorageQueueConfiguration.ConnectionString))
+                throw new ArgumentNullException(nameof(this.azureStorageQueueConfiguration.ConnectionString));
+
+            if (string.IsNullOrWhiteSpace(this.azureStorageQueueConfiguration.QueueName))
+                throw new ArgumentNullException(nameof(this.azureStorageQueueConfiguration.QueueName));
+
+            queueClient = new QueueClient(this.azureStorageQueueConfiguration.ConnectionString, this.azureStorageQueueConfiguration.QueueName);
+        }
+
+        public async Task Queue(Message message)
+        {
+            await queueClient.SendMessageAsync(JsonSerializer.Serialize(message));
+        }
+    }
+}
